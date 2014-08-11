d8dddbca23e81fa5ae1feb979f22ef93b85197a8
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-08-11 07:01:12 +0300

    Update dependencies (clojure-api -> 0.15.0)

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
-                 [metosin/compojure-api "0.14.0"]
+                 [metosin/compojure-api "0.15.0"]
                  [metosin/ring-swagger-ui "2.0.17"]
                  [clj-time "0.7.0"]]
   :plugins [[lein-ring "0.8.10"]]

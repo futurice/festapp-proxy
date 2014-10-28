d3926e66bf0c12c87582181cd04db02df7cfe7cd
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-10-28 12:38:43 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-jetty-adapter "1.3.0"]
-                 [metosin/compojure-api "0.15.0"]
+                 [ring/ring-jetty-adapter "1.3.1"]
+                 [metosin/compojure-api "0.16.2"]
                  [metosin/ring-swagger-ui "2.0.17"]
-                 [clj-time "0.7.0"]]
+                 [clj-time "0.8.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler festapp.web/app
          :reload-paths ["src"]})
